71489b15086666e34e7c7851146477a6de799ced
cyberrasam <rassamrl@gmail.com>
2023-04-21 23:14:59 +0300

    CRUD with dummy database

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,7 @@
 import express from 'express';
-import dotenv from 'dotenv'; 
-import { json } from 'body-parser';
+import dotenv from 'dotenv';
 import morgan from 'morgan';
+import Joi from 'joi'
 
 dotenv.config();
 const port = process.env.PORT || 3000;
@@ -28,10 +28,59 @@ type Planet = {
     },
   ];
 
+  const planetValidator = Joi.object({
+    name: Joi.string().required(),
+  });
+
 app.get('/', (req, res) => {
     res.send("Hello there")
 })
 
+app.get('/api/planets', (req, res) => {
+    res.status(200).json(planets)
+})
+
+app.get('/api/planets/:id', (req, res) => {
+    const {id} = req.params 
+    const planet = planets.find(p => p.id === Number(id))
+    if(planet) {
+        res.status(200).json(planet)
+    } else {
+        res.send("Couldn't find the planet")
+    }
+})
+
+
+app.post('/api/planets', (req, res) => {
+    const id = planets.length + 1; 
+    const {error} = planetValidator.validate(req.body);
+    if(error) {
+        res.status(400).json({ error: error.details.map((err) => err.message) });
+    }
+    const {name} = req.body
+    const newPlanet = {id, name}
+    
+    planets = [...planets, newPlanet] 
+    res.status(201).json({msg: "Planet created successfully"})
+})
+
+app.put('/api/planets/:id', (req,res)=>{
+    const { id } = req.params;
+    const {error} = planetValidator.validate(req.body);
+    if(error) {
+        res.status(400).json({ error: error.details.map((err) => err.message) });
+    }
+    const { name } = req.body; 
+    planets = planets.map(p => p.id === Number(id) ? ({...p, name}) : p ) 
+    res.status(200).json({msg : "Planet is updated"})
+})
+
+app.delete('/api/planets/:id', (req, res) => {
+    const { id } = req.params 
+    planets = planets.filter(p => p.id !== Number(id))
+    res.status(200).json({msg: "planet deleted successfully"})
+    
+})
 
 app.listen(port, () => {
     console.log(`server successfuly launched on http://localhost:${port}`)
